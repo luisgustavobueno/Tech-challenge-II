00044e5575a5062959687293684b46ed5a3d8e53
Luis <luisgustavobuenocolombo@gmail.com>
2024-09-04 08:40:55 -0300

    Bug Fix

--- lib/map/map.py
+++ lib/map/map.py
@@ -12,7 +12,7 @@ class DroneSimulation:
     BLUE = (0, 0, 255)
     ORANGE = (255, 165, 0)
 
-    class MapInfo(Enum):
+    class MAPINFO(Enum):
         OBSTACLE = 1
         DESTINATION = 2
         DRONE_POSITION = 3
@@ -21,7 +21,7 @@ class DroneSimulation:
     def __init__(self, grid_size, cell_size):
         self.grid_size = grid_size
         self.cell_size = cell_size
-        self.grid = [[self.MapInfo.FREE_PATH.value for _ in range(grid_size)] for _ in range(grid_size)]
+        self.grid = [[self.MAPINFO.FREE_PATH.value for _ in range(grid_size)] for _ in range(grid_size)]
         self.delivery_points = []
         self.delivery_order = []
         self.start_position = None
@@ -30,11 +30,11 @@ class DroneSimulation:
         for y in range(self.grid_size):
             for x in range(self.grid_size):
                 rect = pygame.Rect(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size)
-                if self.grid[y][x] == self.MapInfo.OBSTACLE.value:
+                if self.grid[y][x] == self.MAPINFO.OBSTACLE.value:
                     pygame.draw.rect(screen, self.BLACK, rect)
-                elif self.grid[y][x] == self.MapInfo.DESTINATION.value:
+                elif self.grid[y][x] == self.MAPINFO.DESTINATION.value:
                     pygame.draw.rect(screen, self.GREEN, rect)
-                elif self.grid[y][x] == self.MapInfo.DRONE_POSITION.value:
+                elif self.grid[y][x] == self.MAPINFO.DRONE_POSITION.value:
                     pygame.draw.rect(screen, self.BLUE, rect)
                 else:
                     pygame.draw.rect(screen, self.WHITE, rect)
@@ -44,44 +44,48 @@ class DroneSimulation:
         for pos in path:
             for y in range(self.grid_size):
                 for x in range(self.grid_size):
-                    if self.grid[y][x] == self.MapInfo.DRONE_POSITION.value:
-                        self.grid[y][x] = self.MapInfo.FREE_PATH.value
+                    if self.grid[y][x] == self.MAPINFO.DRONE_POSITION.value:
+                        self.grid[y][x] = self.MAPINFO.FREE_PATH.value
                         drone_position = [y, x]
 
             new_position = self.sort_position_to_move(drone_position)
-            self.grid[new_position[0]][new_position[1]] = self.MapInfo.DRONE_POSITION.value
+            self.grid[new_position[0]][new_position[1]] = self.MAPINFO.DRONE_POSITION.value
 
             self.draw_grid(screen)
             pygame.display.flip()
-            time.sleep(0.1)
+            time.sleep(0.3)
 
     @staticmethod
     def clamp_value(value, min_value, max_value):
         return max(min_value, min(value, max_value))
 
     def sort_position_to_move(self, actual_position):
+        new_position = actual_position.copy()  
         while True:
             change_x_or_y = random.randint(0, 1)
-            value_to_change = random.randint(-1, 1)
+            value_to_change = random.choice([-1, 1])
 
-            actual_position[change_x_or_y] += value_to_change
-            actual_position[change_x_or_y] = self.clamp_value(actual_position[change_x_or_y], 0, self.grid_size - 1)
+            new_position[change_x_or_y] += value_to_change
+            new_position[change_x_or_y] = self.clamp_value(new_position[change_x_or_y], 0, self.grid_size - 1)
 
-            value_of_position = self.grid[actual_position[0]][actual_position[1]]
+            value_of_position = self.grid[new_position[0]][new_position[1]]
 
-            if value_of_position != self.MapInfo.OBSTACLE.value:
-                break 
+            if value_of_position != self.MAPINFO.OBSTACLE.value:
+                break
+            else:
+                new_position = actual_position.copy()
 
-        print(f"Posição atual: {actual_position}")
-        return actual_position
+        print(f"Nova posição: {new_position}")
+        return new_position
+
 
 
     def get_delivery_order(self):
         for y in range(self.grid_size):
             for x in range(self.grid_size):
-                if self.grid[x][y] == self.MapInfo.DRONE_POSITION.value:
+                if self.grid[x][y] == self.MAPINFO.DRONE_POSITION.value:
                     self.start_position = [x, y]
-                if self.grid[x][y] == self.MapInfo.DESTINATION.value:
+                if self.grid[x][y] == self.MAPINFO.DESTINATION.value:
                     self.delivery_points.append([x, y])
 
         distancias = [(point, self.calculate_distance(self.start_position, point)) for point in self.delivery_points]
